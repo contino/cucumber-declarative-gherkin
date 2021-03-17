f0fc651850b721fd92a9c0c2005091dd70296b9d
Joe Bailey <joe.bailey@contino.io>
2021-03-16 18:54:17 -0700

    Made the heder logo into a link to home.

--- contino/libs/ui/src/lib/header/header.tsx
+++ contino/libs/ui/src/lib/header/header.tsx
@@ -1,6 +1,10 @@
-import { Box, Grow, Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import React from 'react';
 import AuthButton from '../auth-button/auth-button';
+import {
+  Link
+} from "react-router-dom";
+
 
 import './header.module.scss';
 import { ReactComponent as Logo } from './logo.svg';
@@ -11,10 +15,12 @@ export interface HeaderProps { }
 export function Header(props: HeaderProps) {
   return (
     <header>
-      <Box display="flex" flexWrap="nowrap" flexDirection="row" 
-       alignItems="center" justifyContent="space-between" padding="0 10px">
-        <Logo width="75" height="75" display="flex" />
-        <Typography variant="h1" style={{display:'flex'}}>
+      <Box display="flex" flexWrap="nowrap" flexDirection="row"
+        alignItems="center" justifyContent="space-between" padding="0 10px">
+        <Link to="/">
+          <Logo width="75" height="75" display="flex" />
+        </Link>
+        <Typography variant="h1" style={{ display: 'flex' }}>
           First Change Bank
         </Typography>
         <AuthButton />
